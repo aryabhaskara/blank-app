427fb7dcadece334790c1531de677d2489013f10
Arya Bhaskara <157106561+aryabhaskara@users.noreply.github.com>
2025-11-11 13:08:03 +0000

    ok

--- prediction.py
+++ prediction.py
@@ -2,7 +2,7 @@ import xgboost as xgb
 
 def predict(data):
     model = xgb.XGBClassifier()
-    model.load_model('model/best_model_xgb.model')
-    #model.load_model('model/kf_20.model')
+    #model.load_model('model/best_model_xgb.model')
+    model.load_model('model/kf_20.model')
     #model.load_model('model/gbc_kf20.model')   
     return model.predict(data)
